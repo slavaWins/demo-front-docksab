b4a89e6be1dfc3109c5a789c0bdf7255ac6031b9
slavaWins <zxc7613@mail.ru>
2024-03-18 06:45:18 +0700

    fix angular version

--- AngularService/src/app/app.component.ts
+++ AngularService/src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  imports: [],
+  template: `<h1>Default</h1>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'default';
 }
